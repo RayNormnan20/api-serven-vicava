49464d78b490d5d77bb3d2c96aa2e620be29c31d
RayNormnan20 <josepintadoyamo@gmail.com>
2025-02-21 15:07:49 -0500

    Api version 1

---
+++ serven/src/test/java/api/serven/ServenApplicationTests.java
@@ -0,0 +1,13 @@
+package api.serven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
