8299d3725a98d86f7d6dc03acb841309cca244c6
RayNormnan20 <josepintadoyamo@gmail.com>
2025-03-17 11:43:50 -0500

    --Creación del modulo login

--- frontend_vicava/src/App.js
+++ frontend_vicava/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Route, Routes} from  'react-router-dom';
+import Login from './Components/Login';
+import Registro from './Components/Registro';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Router>
+    <div className='App'>
+      <h1>Serven</h1>
+      <div className='contenedor-principal'>
+      <Routes>
+        {/* Ruta para el  Login*/}
+        <Route  path='/login' element={<Login />}/>
+        {/* Ruta para el  Registro*/}
+        <Route  path='/registro' element={<Registro/>}/>
+      </Routes>
+      </div>
     </div>
+   </Router>
   );
 }
 
