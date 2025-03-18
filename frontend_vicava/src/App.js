114fa3087e03e37302a5fcc408e7f5af459a6d0c
RayNormnan20 <josepintadoyamo@gmail.com>
2025-03-18 11:47:38 -0500

    -- Modificación en la vista principal, Slider, NavBar

--- frontend_vicava/src/App.js
+++ frontend_vicava/src/App.js
@@ -1,24 +1,31 @@
 import './App.css';
-
-import {BrowserRouter as Router, Route, Routes} from  'react-router-dom';
+import './stylesheets/Home.css';
+import './stylesheets/logo.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
 import Registro from './Components/Registro';
+import VerificationPage from './Components/VerificationPage';
+import Home from './views/Home';
+import Layout from './views/Layout'; // Importar Layout
 
 function App() {
   return (
-   <Router>
-    <div className='App'>
-      <h1>Serven</h1>
-      <div className='contenedor-principal'>
-      <Routes>
-        {/* Ruta para el  Login*/}
-        <Route  path='/login' element={<Login />}/>
-        {/* Ruta para el  Registro*/}
-        <Route  path='/registro' element={<Registro/>}/>
-      </Routes>
+    <Router>
+      <div className="App">
+        <Routes>
+          {/* Rutas dentro del Layout */}
+          <Route path="/" element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+         
+          </Route>
+
+          {/* Rutas fuera del Layout */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/registro" element={<Registro />} />
+          <Route path="/verification" element={<VerificationPage />} />
+        </Routes>
       </div>
-    </div>
-   </Router>
+    </Router>
   );
 }
 
